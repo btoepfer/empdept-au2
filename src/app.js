e1d34c4d30d7da715f3a40a62612d4782967d003
Bernd Toepfer <bernd@bc-toepfer.de>
2017-06-28 10:20:17 +0200

    Applikationsgrundgerüst inkl. Navigation

--- src/app.js
+++ src/app.js
@@ -2,4 +2,13 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+
+  configureRouter(config, router) {
+    config.title = 'BCT';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: './resources/elements/welcome',      nav: true, title: 'Welcome' }
+    ]);
+    
+    this.router = router;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import environment from './environment';
+import $ from 'jquery';
+import 'tether';
+import 'bootstrap';
+import './resources/elements/fontawesome5/packs/light'
+import './resources/elements/fontawesome5/packs/regular'
+import './resources/elements/fontawesome5/packs/solid'
+import fontawesome from './resources/elements/fontawesome5/fontawesome';
 
 export function configure(aurelia) {
   aurelia.use
