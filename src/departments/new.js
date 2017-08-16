36a01a19d44295e9f14efbc68924551dc8287561
Bernd Toepfer <bernd@bc-toepfer.de>
2017-08-16 14:21:15 +0200

    Validation on submit

--- src/departments/new.js
+++ src/departments/new.js
@@ -1,35 +1,29 @@
-import { bindable} from 'aurelia-framework';
+import {bindable} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {inject, NewInstance} from 'aurelia-dependency-injection';
 import {EventAggregator} from 'aurelia-event-aggregator';
-import { DepartmentApi } from '../services/department-api';
-import { Department } from '../models/department';
-import {Validator, ValidationRules, ValidationControllerFactory , validateTrigger} from 'aurelia-validation';
+import {DepartmentApi } from '../services/department-api';
+import {Department } from '../models/department';
+import {ValidationRules, ValidationController, validateTrigger} from 'aurelia-validation';
 import {SimpleValidationRenderer} from "../resources/validation/simple-validation-renderer";
 
 
-@inject (Router, Validator, ValidationControllerFactory, DepartmentApi, EventAggregator)
+@inject (Router, NewInstance.of(ValidationController), DepartmentApi, EventAggregator)
 export class New {
 
-  canSave = false;
-
-  constructor(router, validator, controllerFactory, departmentApi, ea) {
+  constructor(router, validationController, departmentApi, ea) {
     this.router = router;
-    this.validator = validator;
     this.departmentApi = departmentApi;
     this.ea = ea;
     this.department = new Department();
-    this.validationController = controllerFactory.createForCurrentScope(validator);
+    this.validationController = validationController;
     this.validationController.addRenderer(new SimpleValidationRenderer());
     // manual triggering of validation
     this.validationController.validateTrigger = validateTrigger.changeOrBlur;
-    this.validationController.subscribe(event => this.validateForm());
+    
+    //this.validationController.subscribe(event => this.validateForm());
   }
 
-  validateForm() {
-    this.validator.validateObject(this.department.attributes)
-      .then(results => this.canSave = results.every(result => result.valid));
-  }
 
   activate() {
     ValidationRules
@@ -44,20 +38,20 @@ export class New {
 
 
   addDepartment() {
-    
-    this.validateForm();
+    let canSave = false;
+    this.validationController.validate()
+      .then(result => canSave=result.valid)
+      .then(() => {     
+        if (canSave) {
+          this.departmentApi.saveDepartment(this.department)
+            .then(department => this.department = department)
+              .then(ea => this.ea.publish('department:created', this.department))
+              .then(() => this.router.navigateToRoute('department', { id: this.department.id }))
+            .catch(err => alert(err.statusText));
+          this.validationController.reset()}
+        });
      
-    if (this.canSave) {
-      this.departmentApi.saveDepartment(this.department)
-        .then(department => this.department = department)
-          .then(ea => this.ea.publish('department:created', this.department))
-          .then(() => this.router.navigateToRoute('department', { id: this.department.id }))
-        .catch(err => alert(err.statusText));
-      this.validationController.reset();
-     
-      console.log(this.department);
-      //this.router.navigateToRoute('department', { id: this.department.id });
-    }
+    console.log(this.department);
   }
 
   attached() {
